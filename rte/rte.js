89d3abc8b0d4427b7190da58a549072f6620e0f9
claudius108 <claudius108@28920dcd-8ef3-4bc1-a661-5e24886d1ffc>
2013-03-07 06:36:11 +0000

    Updated logic for ckeditor.

--- rte/rte.js
+++ rte/rte.js
@@ -10,33 +10,30 @@ exsltforms.rte = {
 		exsltforms.utils.pollingConditions[ 'textarea2rte_register' ] = {
 			testedValue: function() { return ( XsltForms_globals.body != null ) },
 			executedFunction: function() {
-				var textareas = document.getElementsByTagName("textarea");
-				for (var i=0; i < textareas.length; ++i) {
+				var textareas = document.querySelectorAll("textarea[appearance^='exfk:']");
+				var registry = exsltforms.registry;
+				for (var i=0, il = textareas.length; i < il; ++i) {
 					var textarea = textareas[i];
-					if ( textarea.attributes[ 'appearance' ] ) {
-						if ( textarea.attributes[ 'appearance' ].nodeValue.substring(0, 5) == 'exfk:') {
-							var editorID = textarea.attributes[ 'id' ].nodeValue;
-							//alert(editorID);
-							if ( editorID in exsltforms.registry.textarea2rte ) {
-								} else {
-									var clonedEditorID = textarea.attributes[ 'oldid' ].nodeValue;
-									exsltforms.registry.textarea2rte[ editorID ] = {};
-									exsltforms.registry.textarea2rte[ editorID ].editorType = exsltforms.registry.textarea2rte[ clonedEditorID ].editorType;
-									exsltforms.registry.textarea2rte[ editorID ].id = editorID;
-									exsltforms.registry.textarea2rte[ editorID ].XFtextareaID = textarea.parentNode.parentNode.parentNode.parentNode.attributes[ 'id' ].nodeValue;
-									exsltforms.registry.textarea2rte[ editorID ].incremental = exsltforms.registry.textarea2rte[ clonedEditorID ].incremental;
-									exsltforms.registry.textarea2rte[ editorID ].editorContentModified = 'no';
-									exsltforms.registry.textarea2rte[ editorID ].processContentOnSave = exsltforms.rte.generalFunctions.processContentOnSave;
-									exsltforms.registry.textarea2rte[ editorID ].processContentOnUpdate = exsltforms.rte.generalFunctions.processContentOnUpdate;
-									exsltforms.registry.textarea2rte[ editorID ].nativeConfigOptionsObject = exsltforms.registry.textarea2rte[ clonedEditorID ].nativeConfigOptionsObject;
-									exsltforms.registry.textarea2rte[ editorID ].nativeConfigOptionsString = exsltforms.registry.textarea2rte[ clonedEditorID ].nativeConfigOptionsString;
-							}
-							if ( exsltforms.registry.textarea2rte[ editorID ].editorType in { Xinha: 1, DojoEditor: 1, YUIEditor: 1 } ) {
-								} else {
-									exsltforms.rte.specificFunctions[ exsltforms.registry.textarea2rte[ editorID ].editorType ].beforeRendering( exsltforms.registry.textarea2rte[ editorID ].nativeConfigOptionsObject );
-									exsltforms.rte.generateEditor( exsltforms.registry.textarea2rte[ editorID ].editorType, editorID, exsltforms.registry.textarea2rte[ editorID ].nativeConfigOptionsObject, exsltforms.registry.textarea2rte[ editorID ].nativeConfigOptionsString );
-							}
-						}
+					var editorID = textarea.id;
+					var editorType = registry.textarea2rte[ editorID ].editorType;
+					if ( editorID in exsltforms.registry.textarea2rte ) {
+					} else {
+						var clonedEditorID = textarea.attributes[ 'oldid' ].nodeValue;
+						registry.textarea2rte[ editorID ] = {};
+						registry.textarea2rte[ editorID ].editorType = exsltforms.registry.textarea2rte[ clonedEditorID ].editorType;
+						registry.textarea2rte[ editorID ].id = editorID;
+						registry.textarea2rte[ editorID ].XFtextareaID = textarea.parentNode.parentNode.parentNode.parentNode.attributes[ 'id' ].nodeValue;
+						registry.textarea2rte[ editorID ].incremental = exsltforms.registry.textarea2rte[ clonedEditorID ].incremental;
+						registry.textarea2rte[ editorID ].editorContentModified = 'no';
+						registry.textarea2rte[ editorID ].processContentOnSave = exsltforms.rte.generalFunctions.processContentOnSave;
+						registry.textarea2rte[ editorID ].processContentOnUpdate = exsltforms.rte.generalFunctions.processContentOnUpdate;
+						registry.textarea2rte[ editorID ].nativeConfigOptionsObject = exsltforms.registry.textarea2rte[ clonedEditorID ].nativeConfigOptionsObject;
+						registry.textarea2rte[ editorID ].nativeConfigOptionsString = exsltforms.registry.textarea2rte[ clonedEditorID ].nativeConfigOptionsString;
+					}
+					if ( editorType in { Xinha: 1, DojoEditor: 1, YUIEditor: 1 } ) {
+					} else {
+						exsltforms.rte.specificFunctions[editorType].beforeRendering(registry.textarea2rte[editorID].nativeConfigOptionsObject);
+						exsltforms.rte.generateEditor(editorType, editorID, registry.textarea2rte[ editorID ].nativeConfigOptionsObject, registry.textarea2rte[ editorID ].nativeConfigOptionsString );
 					}
 				}
 
@@ -63,42 +60,47 @@ exsltforms.rte = {
 			}
 	}, 
 	//function to generate and render editors one by one
-	generateEditor : function( editorType, editorID, nativeConfigOptionsObject, nativeConfigOptionsString ) {
+	generateEditor : function(editorType, editorID, nativeConfigOptionsObject, nativeConfigOptionsString) {
+			var generalFunctions = exsltforms.rte.generalFunctions;
+			var specificFunctions = exsltforms.rte.specificFunctions;
 				switch ( editorType ) {
 					case "CKEditor":
-						CKEDITOR.replace( document.getElementById( editorID ), nativeConfigOptionsObject );
-						CKEDITOR.instances[ editorID ].on("instanceReady", function() {
-							this.on("blur", function() { exsltforms.rte.generalFunctions.saveEditorContent( editorID, this.document.$.activeElement.innerHTML ); } );
-							if ( exsltforms.registry.textarea2rte[ editorID ].incremental == 'true' ) {
-								this.on("key", function() { exsltforms.rte.generalFunctions.saveEditorContent( editorID, this.document.$.activeElement.innerHTML );});
-								this.on("paste", function() { exsltforms.rte.generalFunctions.saveEditorContent( editorID, this.document.$.activeElement.innerHTML );});
+						CKEDITOR.replace( document.getElementById(editorID), nativeConfigOptionsObject );						
+						CKEDITOR.instances[editorID].on("instanceReady", function() {
+							this.on("blur", function() {exsltforms.rte.generalFunctions.saveEditorContent(editorID, this.document.$.activeElement.innerHTML); } );
+							if (exsltforms.registry.textarea2rte[ editorID ].incremental == 'true') {
+								this.on("key", function() {exsltforms.rte.generalFunctions.saveEditorContent( editorID, this.document.$.activeElement.innerHTML );});
+								this.on("paste", function() {exsltforms.rte.generalFunctions.saveEditorContent( editorID, this.document.$.activeElement.innerHTML );});
 							}
-						});
+						});	
+//						CKEDITOR.instances[editorID].document.on('keyup', function(event) {
+//							alert('a');
+//						});						
 					break;
 					case "TinyMCE":
 						var editorGenerator = new tinymce.Editor( editorID, nativeConfigOptionsObject );
 						if ( exsltforms.registry.textarea2rte[ editorID ].incremental == 'true' ) {
-							editorGenerator.onChange.add(function() { exsltforms.rte.generalFunctions.saveEditorContent( editorID, this.contentDocument.activeElement.innerHTML );});
+							editorGenerator.onChange.add(function() {exsltforms.rte.generalFunctions.saveEditorContent( editorID, this.contentDocument.activeElement.innerHTML );});
 						}
-						editorGenerator.onPostRender.add(function() { exsltforms.rte.specificFunctions.saveTinyMCEContent( this ); });
-						exsltforms.rte.specificFunctions[ exsltforms.registry.textarea2rte[ editorID ].editorType ].beforeRendering( exsltforms.registry.textarea2rte[ editorID ].nativeConfigOptionsObject );
+						editorGenerator.onPostRender.add(function() {exsltforms.rte.specificFunctions.saveTinyMCEContent( this ); });
+						specificFunctions[exsltforms.registry.textarea2rte[editorID].editorType].beforeRendering(exsltforms.registry.textarea2rte[editorID].nativeConfigOptionsObject);
 						editorGenerator.render();
 					break;
 					case "EditArea":
 						var nativeConfigOptionsStringProcessed = nativeConfigOptionsString.substring(nativeConfigOptionsString.indexOf('{') + 1, nativeConfigOptionsString.lastIndexOf('}'));
-						exsltforms.rte.specificFunctions[ exsltforms.registry.textarea2rte[ editorID ].editorType ].beforeRendering( exsltforms.registry.textarea2rte[ editorID ].nativeConfigOptionsObject );
+						specificFunctions[ exsltforms.registry.textarea2rte[ editorID ].editorType ].beforeRendering( exsltforms.registry.textarea2rte[ editorID ].nativeConfigOptionsObject );
 						editAreaLoader.init(eval('(' + '{ id:\"' + editorID + '\", ' + nativeConfigOptionsStringProcessed + ', save_callback: "exsltforms.rte.specificFunctions.saveEditAreaContent"}' + ')'));
 					break;
 					case "YUIEditor":
 // 					exsltforms.utils.pollingConditions[ 'generateYUIEditors' ] = {
 // 						testedValue: function() { return typeof YAHOO.widget.EditorInfo != 'undefined' },
 // 						executedFunction: function() {
-						new YAHOO.widget.Editor( editorID, exsltforms.registry.textarea2rte[ editorID ].nativeConfigOptionsObject );
-						YAHOO.widget.EditorInfo._instances[ editorID ].on( 'editorWindowBlur', function() { exsltforms.rte.generalFunctions.saveEditorContent( editorID, this._getDoc().body.innerHTML ); } );
-						if ( exsltforms.registry.textarea2rte[ editorID ].incremental == 'true' ) {
-							YAHOO.widget.EditorInfo._instances[ editorID ].on('afterNodeChange', function() { exsltforms.rte.generalFunctions.saveEditorContent( editorID, this._getDoc().body.innerHTML ); } );
+						new YAHOO.widget.Editor(editorID, exsltforms.registry.textarea2rte[ editorID ].nativeConfigOptionsObject);
+						YAHOO.widget.EditorInfo._instances[ editorID ].on( 'editorWindowBlur', function() {exsltforms.rte.generalFunctions.saveEditorContent( editorID, this._getDoc().body.innerHTML ); } );
+						if (exsltforms.registry.textarea2rte[ editorID ].incremental == 'true' ) {
+							YAHOO.widget.EditorInfo._instances[ editorID ].on('afterNodeChange', function() {exsltforms.rte.generalFunctions.saveEditorContent( editorID, this._getDoc().body.innerHTML ); } );
 						}
-						exsltforms.rte.specificFunctions[ exsltforms.registry.textarea2rte[ editorID ].editorType ].beforeRendering( exsltforms.registry.textarea2rte[ editorID ].nativeConfigOptionsObject );
+						specificFunctions[exsltforms.registry.textarea2rte[editorID].editorType].beforeRendering( exsltforms.registry.textarea2rte[ editorID ].nativeConfigOptionsObject );
 						YAHOO.widget.EditorInfo._instances[ editorID ].render();
 // 						}
 // 					}
@@ -106,15 +108,15 @@ exsltforms.rte = {
 					break;
 					case "DojoEditor":
 						dojo.addOnLoad(function() {
-							new dijit.Editor( exsltforms.registry.textarea2rte[ editorID ].nativeConfigOptionsObject, dojo.byId( editorID ));
-							dojo.connect(dijit.byId( editorID ), "onBlur", function( editorContent ) { exsltforms.rte.generalFunctions.saveEditorContent( this.id, dijit.registry._hash[ this.id ].document.activeElement.innerHTML ); });
-							if ( exsltforms.registry.textarea2rte[ editorID ].incremental == 'true' ) {
-								dojo.connect( dijit.byId( editorID ), "onNormalizedDisplayChanged", function( event ) { exsltforms.rte.generalFunctions.saveEditorContent( this.id, dijit.registry._hash[ this.id ].document.activeElement.innerHTML ); } );
+							new dijit.Editor(exsltforms.registry.textarea2rte[ editorID ].nativeConfigOptionsObject, dojo.byId( editorID ));
+							dojo.connect(dijit.byId( editorID ), "onBlur", function( editorContent ) {exsltforms.rte.generalFunctions.saveEditorContent( this.id, dijit.registry._hash[ this.id ].document.activeElement.innerHTML ); });
+							if (exsltforms.registry.textarea2rte[ editorID ].incremental == 'true' ) {
+								dojo.connect( dijit.byId( editorID ), "onNormalizedDisplayChanged", function( event ) {exsltforms.rte.generalFunctions.saveEditorContent( this.id, dijit.registry._hash[ this.id ].document.activeElement.innerHTML ); } );
 							}
 						});
 					break;
 				}
-				exsltforms.rte.generalFunctions.addXFupdateListener( editorID );
+				exsltforms.rte.generalFunctions.addXFupdateListener(editorID);
 	},
 	//specific functions for editors
 	specificFunctions : {
@@ -153,15 +155,15 @@ exsltforms.rte = {
 			}
 		},
 		saveDojoContent : function( editorID ) {
-			alert( editorID );
+			
 		}
 	},
 	//general functions for editors
 	generalFunctions : {
 		processContentOnSave	: function( editorContent ) {return editorContent.replace(/&nbsp;/gi, "&#160;");},
 		processContentOnUpdate	: function( editorContent ) {return editorContent;},
-		addXFupdateListener	: function( editorID ) {
-			new XsltForms_listener( document.getElementById( exsltforms.registry.textarea2rte[ editorID ].XFtextareaID ), "xforms-value-changed", null, function( evt ) {
+		addXFupdateListener	: function(editorID) {				
+			new XsltForms_listener(XsltForms_globals.body.xfSubform, document.getElementById(exsltforms.registry.textarea2rte[editorID].XFtextareaID), null, "xforms-value-changed", null, function( evt ) {
 				exsltforms.rte.generalFunctions.updateEditorContent( editorID );
 			});
 		},
@@ -172,18 +174,15 @@ exsltforms.rte = {
 				exsltforms.rte.specificFunctions[ exsltforms.registry.textarea2rte[ editorID ].editorType ].setEditorContent( editorID, document.getElementById( exsltforms.registry.textarea2rte[ editorID ].id ).value );
 				}
 		},
-		saveEditorContent	: function( editorID, editorContent ) {
-			var textareaContent = document.getElementById( editorID ).value;
+		saveEditorContent	: function(editorID, editorContent) {
+			var textareaContent = document.getElementById(editorID).value;
 			setTimeout(
 				function() {
-					if ( textareaContent !== editorContent ) {
+					if (textareaContent !== editorContent) {
 						exsltforms.registry.textarea2rte[ editorID ].editorContentModified = 'yes';
-						var XFtextareaXFElement = document.getElementById( exsltforms.registry.textarea2rte[ editorID ].XFtextareaID ).xfElement;
 						var editorContentProcessedOnSave = exsltforms.registry.textarea2rte[ editorID ].processContentOnSave( editorContent );
-						xforms.openAction();
-						XFtextareaXFElement.valueChanged( editorContentProcessedOnSave || "" );
-						xforms.closeAction();
-					};
+						XsltForms_control.getXFElement(document.getElementById( exsltforms.registry.textarea2rte[ editorID ].XFtextareaID)).valueChanged(editorContentProcessedOnSave || "");
+					}
 				},
 			0);
 		}
